5ce0d5f0fde0d551a225c38d6d98d3c4834fb7d7
KnlnKS <soulkks@hotmail.ca>
2021-07-24 02:16:07 -0400

    🎨 Add Some Style

--- injectContent.js
+++ injectContent.js
@@ -1,23 +1,28 @@
-$("title").replaceWith(
-  `
-      <title>Lever Resume Parsing</title>
-    `
-);
+$("title").replaceWith('<title>Lever Resume Parser - KnlnKS</title>');
 
 $(".content").replaceWith(
-  `
-    <div class="content">
+   `
+   <div class="content">
+      <div class="main-header page-full-width section-wrapper"></div>
+      <div class="section-wrapper accent-section small-accent page-full-width">
+         <div class="section page-centered posting-header">
+            <h2>
+               Lever Resume Parser
+               <div href="https://github.com/KnlnKS" class="sort-by-time posting-category medium-category-label">By KnlnKS</div>
+            </h2>
+            <div class="posting-categories">
+               <a href="https://github.com/KnlnKS/lever-parser-extension" class="sort-by-time posting-category medium-category-label">GitHub&nbsp&nbsp&nbsp/</a>
+               <a href="https://chrome.google.com/webstore/detail/lever-parser-extension/cjaebcdmenhepnpldppdeefjgbbpjjbc" class="sort-by-time posting-category medium-category-label">Chrome Webstore</a>
+            </div>
+         </div>
+      </div>
       <div class="section-wrapper page-full-width">
          <div class="section page-centered application-form">
-            <h4>Parse Resume</h4>
             <label>
                <div class="application-label">Resume/CV</div>
                <div class="application-field">
                   <a href="#" class="postings-btn template-btn-utility visible-resume-upload">
-                     <svg class=" icon icon-paperclip" width="16px" height="16px" viewBox="0 0 16 16">
-                        <path d="M5 5V9C5 10.7 6.3 12 8 12C9.7 12 11 10.7 11 9V4.5C11 2 9 0 6.5 0C4 0 2 2 2 4.5V10C2 13.3 4.  7 16 8 16C11.3 16 14 13.3 14 10V4H12V10C12 12.2 10.2 14 8 14C5.8 14 4 12.2 4 10V4.5C4 3.1 5.1 2 6.5   2C7.9 2 9 3.1 9 4.5V9C9 9.6 8.6 10 8 10C7.4 10 7 9.6 7 9V5H5Z"></path>
-                     </svg>
-                     <span class="filename"></span><span class="default-label">&nbsp&nbsp&nbsp&nbspATTACH RESUME/CV</ span><input class="application-file-input invisible-resume-upload" data-qa="input-resume"  id="resume-upload-input" name="resume" tabindex="-1" type="file">
+                  <span class="filename"></span><span class="default-label">ATTACH RESUME/CV</ span><input class="application-file-input invisible-resume-upload" data-qa="input-resume"  id="resume-upload-input" name="resume" tabindex="-1" type="file">
                   </a>
                   <span class="resume-upload-failure">
                      <div class="resume-upload-label">Couldn't auto-read resume.</div>
@@ -41,6 +46,6 @@ $(".content").replaceWith(
             </span>
          </div>
       </div>
-    </div>
-    `
+   </div>
+   `
 );
